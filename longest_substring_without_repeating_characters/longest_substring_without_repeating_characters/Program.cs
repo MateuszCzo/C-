951119e27b835fbf2e986ed8b2526bc04b905316
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-06-12 23:26:00 +0200

    longest substring without repeating characters

---
+++ longest_substring_without_repeating_characters/longest_substring_without_repeating_characters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longest_substring_without_repeating_characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LengthOfLongestSubstring("abcabcbb");
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int leftIndex = 0, result = 0;
+            // character -> index
+            IDictionary<char, int> characterIndex = new Dictionary<char, int>();
+
+            for (int rightIndex = 0; rightIndex < s.Length; rightIndex++)
+            {
+                if (characterIndex.TryGetValue(s[rightIndex], out int oldIndex) && leftIndex <= oldIndex)
+                {
+                    leftIndex = oldIndex + 1;
+                }
+                characterIndex[s[rightIndex]] = rightIndex;
+                result = Math.Max(result, rightIndex - leftIndex + 1);
+            }
+
+            return result;
+        }
+    }
+}
