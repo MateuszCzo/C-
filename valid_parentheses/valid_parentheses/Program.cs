a8f8b6e524339ecdc2b969af5732b54ddb46e83c
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-06-12 23:26:47 +0200

    valid parentheses

---
+++ valid_parentheses/valid_parentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valid_parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool IsValid(string s)
+        {
+            IDictionary<char, char> dictionary = new Dictionary<char, char> {
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' },
+            };
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dictionary.TryGetValue(s[i], out char character))
+                {
+                    if (stack.Count == 0 || stack.Pop() != character)
+                    {
+                        return false;
+                    }
+                } else
+                {
+                    stack.Push(s[i]);
+                }
+            }
+            if (stack.Count != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
