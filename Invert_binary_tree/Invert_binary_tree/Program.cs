5fae543eeb6c04d74eac085106ff78eedc599173
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-06-12 23:24:41 +0200

    invert binary tree

---
+++ Invert_binary_tree/Invert_binary_tree/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invert_binary_tree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
